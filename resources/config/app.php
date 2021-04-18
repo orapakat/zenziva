81e33654ff7e6b44a37bf8e4174069ccb691acf6
educms <educmsofficial@gmail.com>
2021-04-18 23:50:08 +0700

    update release

--- resources/config/app.php
+++ resources/config/app.php
@@ -1,23 +1,10 @@
 <?php
 return [
 'providers' => [
-
-/*
- * Laravel Framework Service Providers...
- */
-Illuminate\Auth\AuthServiceProvider::class,
-
-//
-
 /*
  * Package Service Providers...
  */
 Orapakat\Zenzivadev\Providers\ZenzivaODVServiceProvider::class,
-
-/*
- * Application Service Providers...
- */
-App\Providers\AppServiceProvider::class,
 
 //
 ],
