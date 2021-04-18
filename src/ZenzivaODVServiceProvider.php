bc42aaeca5265bc99a7cfcdbfb625ab04036d490
educms <educmsofficial@gmail.com>
2021-04-19 00:46:09 +0700

    update v2.1

---
+++ src/ZenzivaODVServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Orapakat\Zenzivadev\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ZenzivaODVServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../resources/config/orapakatdev_zenziva.php' => config_path('orapakatdev_zenziva.php'),
+        ]);
+    }
+}
